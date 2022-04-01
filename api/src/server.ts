f52729a1609ccdcb666f0ccc008f0d6a06400109
Felipe Araujo <felipe.ar@pm.me>
2022-04-01 18:35:25 -0300

    heroku start

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,4 +1,4 @@
-import * as express from 'express';
+import express from 'express';
  
 function loggerMiddleware(request: express.Request, response: express.Response, next) {
   console.log(`${request.method} ${request.path}`);
