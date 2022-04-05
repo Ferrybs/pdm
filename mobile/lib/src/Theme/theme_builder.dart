9940e50998ade2580a41aa545c1f3b9b2b9d5b8f
Rapharrel <rafaelbarbosa525@gmail.com>
2022-04-05 04:18:01 -0300

    Add light and dark theme on login and register views

---
+++ mobile/lib/src/Theme/theme_builder.dart
@@ -0,0 +1,217 @@
+import 'package:flutter/material.dart';
+
+class ThemeBuilder {
+  final ThemeData _baseTheme;
+  final ColorScheme _colorScheme;
+  ThemeBuilder(this._baseTheme, this._colorScheme);
+  getTheme() {
+    return _baseTheme.copyWith(
+      colorScheme: _colorScheme,
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+      appBarTheme: appBarTheme,
+      textTheme: textTheme,
+      textSelectionTheme: textSelectionTheme,
+      inputDecorationTheme: inputDecorationTheme,
+      elevatedButtonTheme: elevatedButtonTheme,
+      textButtonTheme: textButtonTheme,
+    );
+  }
+
+  get textButtonTheme {
+    return TextButtonThemeData(
+        style: ButtonStyle(
+      foregroundColor: MaterialStateProperty.all(_colorScheme.tertiary),
+    ));
+  }
+
+  get elevatedButtonTheme {
+    return ElevatedButtonThemeData(
+      style: ButtonStyle(
+          overlayColor: MaterialStateProperty.all(
+            _colorScheme.tertiary.withAlpha(128),
+          ),
+          backgroundColor: MaterialStateProperty.all(
+            _colorScheme.tertiary,
+          ),
+          foregroundColor: MaterialStateProperty.all(
+            _colorScheme.onTertiary,
+          ),
+          minimumSize: MaterialStateProperty.all(
+            const Size(200.0, 60.0),
+          ),
+          fixedSize: MaterialStateProperty.all(
+            const Size.fromHeight(60.0),
+          ),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(50.0)),
+          ),
+          padding: MaterialStateProperty.all(
+            const EdgeInsets.symmetric(vertical: 16.0),
+          )),
+    );
+  }
+
+  get inputDecorationTheme {
+    return _baseTheme.inputDecorationTheme.copyWith(
+      iconColor: _colorScheme.onTertiary,
+      border: InputBorder.none,
+      fillColor: _colorScheme.tertiary,
+      filled: true,
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: _colorScheme.onTertiary,
+          width: 2.0,
+        ),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(20.0),
+        ),
+      ),
+    );
+  }
+
+  get textSelectionTheme {
+    return _baseTheme.textSelectionTheme.copyWith(
+        cursorColor: _colorScheme.onTertiary,
+        selectionColor: _colorScheme.onTertiary,
+        selectionHandleColor: _colorScheme.onTertiary);
+  }
+
+  get appBarTheme {
+    return _baseTheme.appBarTheme.copyWith(
+      backgroundColor: _colorScheme.background,
+    );
+  }
+
+  get bodyLarge {
+    return _baseTheme.textTheme.bodyLarge?.copyWith(
+      fontFamily: "Poppins",
+    );
+  }
+
+  get bodyMedium {
+    return _baseTheme.textTheme.bodyMedium?.copyWith(
+      color: _colorScheme.secondary,
+      fontFamily: "Poppins",
+    );
+  }
+
+  get bodySmall {
+    return _baseTheme.textTheme.bodySmall?.copyWith(
+      fontFamily: "Poppins",
+    );
+  }
+
+  get displayLarge {
+    return _baseTheme.textTheme.displayLarge?.copyWith(
+      fontFamily: "Poppins",
+    );
+  }
+
+  get displayMedium {
+    return _baseTheme.textTheme.titleMedium?.copyWith(
+      fontFamily: "Poppins",
+    );
+  }
+
+  get displaySmall {
+    return _baseTheme.textTheme.displaySmall?.copyWith(
+      fontFamily: "Poppins",
+    );
+  }
+
+  get headlineLarge {
+    return _baseTheme.textTheme.headlineLarge?.copyWith(
+      fontFamily: "Poppins",
+    );
+  }
+
+  get headlineMedium {
+    return _baseTheme.textTheme.headlineMedium?.copyWith(
+      fontFamily: "Poppins",
+    );
+  }
+
+  get headlineSmall {
+    return _baseTheme.textTheme.headlineSmall?.copyWith(
+      fontFamily: "Poppins",
+    );
+  }
+
+  get labelLarge {
+    return _baseTheme.textTheme.labelLarge?.copyWith(
+      fontFamily: "Poppins",
+    );
+  }
+
+  get labelMedium {
+    return _baseTheme.textTheme.labelMedium?.copyWith(
+      fontFamily: "Poppins",
+    );
+  }
+
+  get labelSmall {
+    return _baseTheme.textTheme.labelSmall?.copyWith(
+      fontFamily: "Poppins",
+    );
+  }
+
+  get titleLarge {
+    return _baseTheme.textTheme.titleLarge?.copyWith(
+      fontFamily: "Poppins",
+      fontWeight: FontWeight.w600,
+      color: _colorScheme.tertiary,
+      fontSize: 20,
+    );
+  }
+
+  get titleMedium {
+    return _baseTheme.textTheme.titleMedium?.copyWith(
+      color: _colorScheme.secondary,
+      fontFamily: "Poppins",
+    );
+  }
+
+  get titleSmall {
+    return _baseTheme.textTheme.titleMedium?.copyWith(
+      fontFamily: "Poppins",
+    );
+  }
+
+  get textTheme {
+    return _baseTheme.textTheme.copyWith(
+      // Material 3 (2021) formats
+      displayLarge: displayLarge,
+      displayMedium: displayMedium,
+      displaySmall: displaySmall,
+      headlineLarge: headlineLarge,
+      headlineMedium: headlineMedium,
+      headlineSmall: headlineSmall,
+      titleLarge: titleLarge,
+      titleMedium: titleMedium,
+      titleSmall: titleSmall,
+      bodyLarge: bodyLarge,
+      bodyMedium: bodyMedium,
+      bodySmall: bodySmall,
+      labelLarge: labelLarge,
+      labelMedium: labelMedium,
+      labelSmall: labelSmall,
+
+      // Material 2 formats (Deprecated)
+      // display2 = DisplayLarge
+      // display3 = DisplayMedium
+      // headline1 = DisplaySmall
+      // headline2 = HeadlineLarge
+      // headline3 = HeadlineMedium
+      // headline4 = HeadlineSmall
+      // headline5 = TitleLarge
+      // subhead1/Subtitle1 = TitleMedium
+      // subhead2/Subtitle2 = TitleSmall
+      // body1 = BodyLarge
+      // body2 = BodyMedium
+      // caption = BodySmall
+      // button = LabelLarge
+      // overline = LabelMedium
+      // N/A = LabelSmall
+    );
+  }
+}
--- mobile/lib/src/features/auth/presentation/view/page/create_account.dart
+++ mobile/lib/src/features/auth/presentation/view/page/create_account.dart
@@ -1,8 +1,7 @@
 import 'package:basearch/components/hud/modal_progress_hud.dart';
-import 'package:basearch/components/button/rounded_btn.dart';
+import 'package:basearch/src/Theme/theme.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:localization/localization.dart';
 
 class CreateAccount extends StatefulWidget {
@@ -16,217 +15,185 @@ class _CreateAccount extends State<CreateAccount> {
   bool showSpinner = false;
   @override
   Widget build(BuildContext context) {
-    return ModalProgressHUD(
-        inAsyncCall: showSpinner,
-        child: Scaffold(
-          appBar: AppBar(
-            elevation: 0,
-            leading: _goBackButton(context),
-            backgroundColor: Color(0xff251F34),
-          ),
-          backgroundColor: Color(0xff251F34),
-          body: SingleChildScrollView(
-              child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(20, 20, 20, 8),
-                child: Text(
-                  'create-account'.i18n(),
-                  style: const TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 25),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: Text(
-                  'fill-input'.i18n(),
-                  style: TextStyle(
-                      color: Colors.grey[600],
-                      fontWeight: FontWeight.w400,
-                      fontSize: 14),
-                ),
-              ),
-              Container(
-                margin:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+    return MaterialApp(
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      home: ModalProgressHUD(
+          inAsyncCall: showSpinner,
+          child: Scaffold(
+            appBar: AppBar(
+              elevation: 0,
+              leading: _goBackButton(context),
+            ),
+            body: SingleChildScrollView(
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'name'.i18n(),
-                      style: const TextStyle(
-                          fontWeight: FontWeight.w300,
-                          fontSize: 13,
-                          color: Colors.white),
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 20, 20, 8),
+                  child: Builder(
+                    builder: (context) => Text(
+                      'create-account'.i18n(),
+                      style: Theme.of(context).textTheme.titleLarge,
                     ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    TextField(
-                      style: (const TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.w400)),
-                      keyboardType: TextInputType.emailAddress,
-                      obscureText: false,
-                      cursorColor: Colors.white,
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        fillColor: Color(0xfff3B324E),
-                        filled: true,
-                        prefixIcon: Icon(
-                            IconData(0xf0071, fontFamily: 'MaterialIcons')),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide:
-                              BorderSide(color: Color(0xff14DAE2), width: 2.0),
-                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                        ),
-                      ),
-                      onChanged: (value) {},
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      'last-name'.i18n(),
-                      style: const TextStyle(
-                          fontWeight: FontWeight.w300,
-                          fontSize: 13,
-                          color: Colors.white),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    TextField(
-                      style: (const TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.w400)),
-                      keyboardType: TextInputType.emailAddress,
-                      obscureText: false,
-                      cursorColor: Colors.white,
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        fillColor: Color(0xfff3B324E),
-                        filled: true,
-                        prefixIcon: Icon(
-                            IconData(0xf0071, fontFamily: 'MaterialIcons')),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide:
-                              BorderSide(color: Color(0xff14DAE2), width: 2.0),
-                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                        ),
-                      ),
-                      onChanged: (value) {},
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    const Text(
-                      'E-mail',
-                      style: TextStyle(
-                          fontWeight: FontWeight.w300,
-                          fontSize: 13,
-                          color: Colors.white),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    TextField(
-                      style: (const TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.w400)),
-                      keyboardType: TextInputType.emailAddress,
-                      obscureText: false,
-                      cursorColor: Colors.white,
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        fillColor: Color(0xfff3B324E),
-                        filled: true,
-                        prefixIcon:
-                            Icon(IconData(0xe780, fontFamily: 'MaterialIcons')),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide:
-                              BorderSide(color: Color(0xff14DAE2), width: 2.0),
-                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                        ),
-                      ),
-                      onChanged: (value) {},
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                margin:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'password'.i18n(),
-                      style: const TextStyle(
-                          fontWeight: FontWeight.w300,
-                          fontSize: 13,
-                          color: Colors.white),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    TextField(
-                      style: (const TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.w400)),
-                      obscureText: true,
-                      cursorColor: Colors.white,
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        fillColor: Color(0xfff3B324E),
-                        filled: true,
-                        prefixIcon:
-                            Icon(IconData(0xeb71, fontFamily: 'MaterialIcons')),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide:
-                              BorderSide(color: Color(0xff14DAE2), width: 2.0),
-                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                        ),
-                      ),
-                      onChanged: (value) {},
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Center(
-                  child: RoundedButton(
-                    btnText: 'sign-up'.i18n().toUpperCase(),
-                    color: Color(0xff14DAE2),
-                    onPressed: () {},
-                    textColor: Color(0xffC5D8A4),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'have-account'.i18n(),
-                    style: TextStyle(
-                        color: Colors.grey[600], fontWeight: FontWeight.w400),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Builder(
+                    builder: (context) => Text(
+                      'fill-input'.i18n(),
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
                   ),
-                  TextButton(
-                    onPressed: () {
-                      Modular.to.navigate('/');
-                    },
-                    child: Text('sign-in'.i18n(),
-                        style: const TextStyle(
-                          color: Color(0xff14DAE2),
-                        )),
-                  )
-                ],
-              ),
-            ],
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 20,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('name'.i18n(),
+                          style: Theme.of(context).textTheme.labelLarge),
+                      const SizedBox(height: 10),
+                      Builder(
+                          builder: (context) => TextField(
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleMedium
+                                    ?.copyWith(
+                                        color: Theme.of(context)
+                                            .textSelectionTheme
+                                            .cursorColor),
+                                keyboardType: TextInputType.emailAddress,
+                                obscureText: false,
+                                decoration: const InputDecoration(
+                                  prefixIcon: Icon(
+                                    IconData(0xf0071,
+                                        fontFamily: 'MaterialIcons'),
+                                  ),
+                                ),
+                                onChanged: (value) {},
+                              )),
+                      const SizedBox(height: 10),
+                      Text(
+                        'last-name'.i18n(),
+                        style: Theme.of(context).textTheme.labelLarge,
+                      ),
+                      const SizedBox(height: 10),
+                      Builder(
+                          builder: (context) => TextField(
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleMedium
+                                    ?.copyWith(
+                                        color: Theme.of(context)
+                                            .textSelectionTheme
+                                            .cursorColor),
+                                keyboardType: TextInputType.emailAddress,
+                                obscureText: false,
+                                decoration: const InputDecoration(
+                                  prefixIcon: Icon(
+                                    IconData(0xf0071,
+                                        fontFamily: 'MaterialIcons'),
+                                  ),
+                                ),
+                                onChanged: (value) {},
+                              )),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text('E-mail',
+                          style: Theme.of(context).textTheme.labelLarge),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Builder(
+                          builder: (context) => TextField(
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleMedium
+                                    ?.copyWith(
+                                        color: Theme.of(context)
+                                            .textSelectionTheme
+                                            .cursorColor),
+                                keyboardType: TextInputType.emailAddress,
+                                obscureText: false,
+                                decoration: const InputDecoration(
+                                  prefixIcon: Icon(
+                                    IconData(0xe780,
+                                        fontFamily: 'MaterialIcons'),
+                                  ),
+                                ),
+                                onChanged: (value) {},
+                              )),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Builder(
+                        builder: (context) => Text(
+                          'password'.i18n(),
+                          style: Theme.of(context).textTheme.labelLarge,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Builder(
+                          builder: (context) => TextField(
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleMedium
+                                    ?.copyWith(
+                                        color: Theme.of(context)
+                                            .textSelectionTheme
+                                            .cursorColor),
+                                obscureText: true,
+                                decoration: const InputDecoration(
+                                  prefixIcon: Icon(
+                                    IconData(0xeb71,
+                                        fontFamily: 'MaterialIcons'),
+                                  ),
+                                ),
+                                onChanged: (value) {},
+                              )),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(
+                    child: ElevatedButton(
+                      child: Text('sign-up'.i18n().toUpperCase()),
+                      onPressed: () {},
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Builder(
+                      builder: (context) => Text(
+                        'have-account'.i18n(),
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () => Modular.to.navigate('/'),
+                      child: Text('sign-in'.i18n()),
+                    )
+                  ],
+                ),
+              ],
+            )),
           )),
-        ));
+    );
   }
 }
 
