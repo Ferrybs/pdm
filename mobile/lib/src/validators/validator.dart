b45c018870b26f316fcf166626350fffa9b429ff
Leonardo Miranda <leonardo_miranda@icloud.com>
2022-04-24 23:44:43 -0300

    validation modified

--- mobile/lib/src/validators/validator.dart
+++ mobile/lib/src/validators/validator.dart
@@ -1,7 +1,8 @@
 extension ValidatorString on String {
   static final regxEmail = RegExp(r"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
   static final regxPassword = RegExp(r"(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}");
-  static final regxPerson = RegExp(r"^[A-Za-z]+(((\'|\-|\.)?([A-Za-z])+))?$");
+  static final regxPerson =
+      RegExp(r"^[A-Za-z]+(((\'|\-|\.|\ )?([A-Za-z])+))?$");
   bool isEmail() {
     return regxEmail.hasMatch(this);
   }
