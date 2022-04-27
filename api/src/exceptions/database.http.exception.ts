d1546f04b57a7b0a442eee90cf09dda44fc0aaf0
Leonardo Miranda <leonardo_miranda@icloud.com>
2022-04-27 20:25:47 -0300

    session exception added and database exception modified

--- api/src/exceptions/database.http.exception.ts
+++ api/src/exceptions/database.http.exception.ts
@@ -1,7 +1,6 @@
 import HttpException from "./http.exceptions";
 
 class DatabaseHttpException extends HttpException{
-    message: string;
     constructor(message: string){
         super(500,`Database error: ${message}`)
 }
