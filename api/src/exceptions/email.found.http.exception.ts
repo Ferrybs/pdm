8842d01f1d47bdb14bef57a04bb03c777816802a
Leonardo Miranda <leonardo_miranda@icloud.com>
2022-04-27 21:41:19 -0300

    email not send exception created and other exceptions formatted

--- api/src/exceptions/email.found.http.exception.ts
+++ api/src/exceptions/email.found.http.exception.ts
@@ -3,7 +3,7 @@ import HttpException from "./http.exceptions";
 class EmailFoundHttpException extends HttpException{
     constructor(email: string){
         super(403,`Client with email ${email} already exists`)
-}
+    }
 }
 
 export default EmailFoundHttpException;
--- api/src/exceptions/hash.http.exception.ts
+++ api/src/exceptions/hash.http.exception.ts
@@ -4,7 +4,7 @@ class HashHttpException extends HttpException{
     message: string;
     constructor(message: string){
         super(500,`Failed to hash: ${message}`)
-}
+    }
 }
 
 export default HashHttpException;
