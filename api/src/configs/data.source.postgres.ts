4822c516c50274f3328eb9c9908a5c5ac56e183f
Felipe Araujo <felipe.ar@pm.me>
2022-06-02 21:20:54 -0300

    postgres config

--- api/src/configs/data.source.postgres.ts
+++ api/src/configs/data.source.postgres.ts
@@ -4,15 +4,15 @@ import Person from "../entity/person.entity";
 import Credentials from "../entity/credentials.entity";
 import Client from "../entity/client.entity";
 import Session from "../entity/session.entity";
-import Device from "../features/device/entity/device.entity";
-import Measure from "../features/device/entity/measure.entity";
-import TypeMeasure from "../features/device/entity/type.measure.entity";
 import TypeSession from "../entity/type.session.entity";
-import DeviceLocalization from "../features/device/entity/device.localization.entity";
-import DevicePreferences from "../features/device/entity/device.preferences.entity";
-import ChatbotSession from "../features/chatbot/entity/chatbot.session.entity";
-import ChatbotMessage from "../features/chatbot/entity/chatbot.message.entity";
-import ChatbotTypeMessage from "../features/chatbot/entity/chatbot.type.message.entity";
+import Device from "../features/device/entities/device.entity";
+import Measure from "../features/device/entities/measure.entity";
+import TypeMeasure from "../features/device/entities/type.measure.entity";
+import DeviceLocalization from "../features/device/entities/device.localization.entity";
+import DevicePreferences from "../features/device/entities/device.preferences.entity";
+import ChatbotSession from "../features/chatbot/entities/chatbot.session.entity";
+import ChatbotMessage from "../features/chatbot/entities/chatbot.message.entity";
+import ChatbotTypeMessage from "../features/chatbot/entities/chatbot.type.message.entity";
 
 export default class PostgresDataSource{
     _appDataSource: DataSource;
